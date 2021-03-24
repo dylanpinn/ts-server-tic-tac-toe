aefa1c7d667eedfb9cf3a91453a5ed0487a70499
Dylan Pinn <me@dylanpinn.com>
2021-03-24 22:31:22 +1100

    Remove example test

--- cypress/integration/example.spec.ts
+++
@@ -1,9 +0,0 @@
-type Url = string;
-
-describe("Example spec", () => {
-  it("works", () => {
-    const url: Url = "https://example.cypress.io";
-    cy.visit(url);
-    cy.contains("Kitchen Sink");
-  });
-});
