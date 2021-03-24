38d1802b16ce081ea4757591441724ee465c5347
Dylan Pinn <me@dylanpinn.com>
2021-03-25 00:10:21 +1100

    Render React code

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import * as Server from "react-dom/server";
 
-let Greet = () => <h1>Hello, world!</h1>;
-console.log(Server.renderToString(<Greet />));
+export default function App() {
+  return <p>Hi</p>;
+}
